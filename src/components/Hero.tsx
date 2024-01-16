1d9d56c2af5966e2bd7817e78b72ff9dc7b97e4a
Danny Ryan <dannykryan@gmail.com>
2024-01-16 12:26:07 +0000

    added Navbar and hover styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Hero = () => {
   return (
-    <div className="bg-[url()] lg:bg-[url(/banner_bg.jpg)] min-h-screen bg-cover"></div>
+    <div className="bg-[url()] lg:bg-[url(/banner_bg.jpg)] min-h-screen bg-cover">
+        <Navbar />
+    </div>
   )
 }
 
