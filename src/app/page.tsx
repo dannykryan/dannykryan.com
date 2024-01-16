faa8f49edd92870298482e3a5670f3eebba1a205
Danny Ryan <dannykryan@gmail.com>
2024-01-16 13:22:55 +0000

    added My Projects dummy section with basic styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from '@/components/Hero'
 import Image from 'next/image'
+import Projects from '@/components/Projects'
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Projects />
     </main>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,6 +5,16 @@ const Hero = () => {
   return (
     <div className="bg-[url()] lg:bg-[url(/banner_bg.jpg)] min-h-screen bg-cover">
         <Navbar />
+        <div className="container grid lg:grid-cols-2 h-[calc(100vh-60px)]">
+            <div className="hidden lg:block"></div>    
+            <div className="text-[100px] sm:text-[130px] font-bold leading-tight flex justify-center items-center">
+                <div>
+                    <p>I&apos;m</p>
+                    <p>Danny</p>
+                    <p>Ryan</p>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
