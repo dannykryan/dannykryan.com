2f647c0548c2ca128676da66724a12852c5ce318
Danny Ryan <dannykryan@gmail.com>
2026-02-10 12:47:03 +0000

    udpate project details and images

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,45 +3,45 @@ import Heading from './Heading'
 import Card from './Card'
 
 const projectData = [
-    {
+  {
       id: 0,
+      title: "Budget Reporting Tool for Harvest",
+      desc: "Generate comprehensive budget reports for Harvest projects by aggregating time entries, costs, and roles into a Google Sheet.",
+      img: "/harvest-budget-report.png",
+      tags: ["Google Apps Script", "Javascript", "Harvest API"],
+      link: "https://github.com/dannykryan/harvest-budget-report"
+    },
+    {
+      id: 1,
+      title: "Team Capacity Reporting Dashboard",
+      desc: "Provides real-time insights into team capacity for leave, holidays and asssigned projects by integrating with Monday.com, LeaveDates, and Harvest APIs.",
+      img: "/team-capacity-dashboard.png",
+      tags: ["Harvest API", "LeaveDates API", "Monday API"],
+      link: "https://github.com/dannykryan/team-capacity-reporting-dashboard"
+    },
+    {
+      id: 2,
       title: "Habitap",
       desc: "A Next.js v14 project that uses Supabase to help you to track your habits and scores you for your ability to stick to them.",
       img: "/habitap-bg.png",
       tags: ["React", "Next.js", "CSS", "Supabase"],
-      link: "https://habitap.vercel.app/",
+      link: "https://github.com/dannykryan/habitap",
     },
     {
-      id: 1,
+      id: 3,
       title: "Study Buddy",
       desc: "A web app built with vanilla HTML, CSS and JS for students to share resources with individual user accounts.",
       img: "/studybuddy-bg.png",
       tags: ["HTML", "CSS", "JS", "SQL"],
-      link: "https://study-buddy-9en0.onrender.com/",
+      link: "https://github.com/dannykryan/studybuddy",
     },
     {
-      id: 2,
+      id: 4,
       title: "Rise Therapies Website",
       desc: "A vanilla HTML and CSS website for Rise Maternity Therapies that included logo and branding design.",
       img: "/rise-therapies-bg.png",
       tags: ["HTML", "CSS"],
-      link: "https://rise-therapies.co.uk/",
-    },
-    {
-      id: 3,
-      title: "Weather App",
-      desc: "A weather app that uses APIs to get the weather of any city or town in the world and updates images based on the weather and time of day.",
-      img: "/weather-app-bg.png",
-      tags: ["HTML", "CSS", "JS", "API"],
-      link: "https://github.com/dannykryan/soc3-weatherApp"
-    },
-    {
-      id: 4,
-      title: "Harvest Budget Reporting App",
-      desc: "A Google Apps Script tool that generates budget reports for Harvest projects by aggregating time entries, costs, and roles into a Google Sheet.",
-      img: "/weather-app-bg.png",
-      tags: ["Google Apps Script", "Javascript", "Harvest API"],
-      link: "https://github.com/dannykryan/soc3-weatherApp"
+      link: "https://github.com/dannykryan/rise-therapies",
     },
   ];
 
